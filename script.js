492b030128420ac901cfaff2cd5b4517d1b86ccc
Takeo2025 <rikiikeda@dornier.com>
2025-11-20 10:12:49 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const pwcreate = document.querySelector('.create'); // ボタン
+const pwresult = document.querySelector('.result'); // input
+const pwcopy  = document.querySelector('.copy'); //コピー
+
+// ボタンをクリックしたらパスワード生成
+pwcreate.addEventListener('click', () => {
+   
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    const numbers = '1234567890';
+    const symbols = '!@#$%&*-_+=';
+    const allletters = letters + numbers + symbols;
+    
+    let password = '';
+
+    //必ず特殊記号を入れる
+    const symbolIndex = Math.floor(Math.random() * symbols.length);
+    password += symbols[symbolIndex]
+
+    //残りの数字をランダムで取得
+    for (let i = 0; i < 5; i++) { // 5文字のパスワード
+    const randomIndex = Math.floor(Math.random() * allletters.length);
+    password += allletters[randomIndex]; 
+}
+    console.log(password); // 例: "A3B1C2"
+    pwresult.value = password;
+});
+
+
+//コピーを押したときの動作
+pwcopy.addEventListener('click', () => {
+    const copyDo = pwresult.value;
+    navigator.clipboard.writeText(copyDo);
+    window.alert('コピーしました!');
+});
